e275fe9e3922f9d2009329d45f321d43c841c2f3
Thomas Björk <107463939+codedeviate@users.noreply.github.com>
2023-07-28 08:07:55 +0200

    Cleanup of script rstat

--- .nrg/scripts/rstat.js
+++ .nrg/scripts/rstat.js
@@ -3,22 +3,7 @@ include("lib/fs.js")
 include("lib/strings.js")
 include("lib/git.js")
 
-// const basepath = cwd()
-// const stats = []
-// let labelWidth = 25
-
 print("Finding git projects...")
-// const s = runcmdstr("find . -name '.git'")
-// const data = trim(s[0])
-//
-// if(data.length == 0) {
-//     println("\rNo git projects found         ")
-//     exit(0)
-// }
-//
-// const paths = data.split("\n").sort(function (a, b) {
-//     return a.toLowerCase().localeCompare(b.toLowerCase());
-// })
 const paths = sortstrings(glob("**/.git"))
 
 if(paths.length == 0) {
@@ -29,34 +14,8 @@ if(paths.length == 0) {
 println("\rFound " + paths.length + " git projects   ")
 println("=".repeat(("Found " + " git projects").length + itoa(paths.length).length))
 
-// paths.forEach(path => {
-//     if(path.length < 5){
-//         return
-//     }
-//     const gitpath = path.substring(0, path.length - 5)
-//     cd(gitpath)
-//     response = runcmdstr("git diff --shortstat");
-//     diff = trimwhitespace(response[0])
-//     if(diff == ""){
-//         diff = "No change"
-//     }
-//     response = runcmdstr("git branch --show-current");
-//     branch = trimwhitespace(response[0])
-//     stats.push({
-//         name: gitpath,
-//         branch: branch,
-//         diff: diff
-//     })
-//     cd(basepath)
-// })
 const stats = gitpaths(paths)
 
-// stats.forEach(element => {
-//     let width = element.name.length + element.branch.length + 6
-//     if(width > labelWidth){
-//         labelWidth = width
-//     }
-// });
 const labelWidth = longeststring(stats.map(element => element.name + element.branch)) + 6
 
 // Print it all out
